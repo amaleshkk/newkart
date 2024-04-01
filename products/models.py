615067140364e64498021f06add491e7dd79022d
amaleshkk <amaleshkk@gmail.com>
2024-04-01 21:26:20 +0530

    added customer and product details

---
+++ products/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+# Model for product
+
+class Product(models.Model):
+    LIVE = 1
+    DELETE = 0
+    DELETE_STATUS = ((LIVE, 'Live'), (DELETE, 'Delete'))
+    title = models.CharField(max_length=50)
+    price = models.FloatField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='/media')
+    priority = models.IntegerField(default=0)
+    delete_status = models.IntegerField(choices=DELETE_STATUS, default=LIVE)
+    create_time = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self) -> str:
+        return self.title 
