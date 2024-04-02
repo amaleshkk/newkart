a03e504b5f381f29a9a777209b5799cd0d349019
amaleshkk <amaleshkk@gmail.com>
2024-04-02 19:24:34 +0530

    data model design

--- products/models.py
+++ products/models.py
@@ -8,10 +8,11 @@ class Product(models.Model):
     LIVE = 1
     DELETE = 0
     DELETE_STATUS = ((LIVE, 'Live'), (DELETE, 'Delete'))
+
     title = models.CharField(max_length=50)
     price = models.FloatField()
     description = models.TextField()
-    image = models.ImageField(upload_to='/media')
+    image = models.ImageField(upload_to='media/')
     priority = models.IntegerField(default=0)
     delete_status = models.IntegerField(choices=DELETE_STATUS, default=LIVE)
     create_time = models.DateTimeField(auto_now_add = True)
